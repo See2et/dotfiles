f457fc42b62dc76d47351080534744c450fea8d0
see2et <seezet1024@gmail.com>
2023-02-25 06:07:52 +0900

    apply: lazy load for many plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
 require('plugins')
 require('base')
 require('maps')
+require('terminal')
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -18,13 +18,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
+  -- Package manager itself
+  { "folke/lazy.nvim", tag = "stable" },
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
+    lazy = true,
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
+      {'williamboman/mason.nvim'},
       'williamboman/mason-lspconfig.nvim',
 
       -- Useful status updates for LSP
@@ -56,11 +59,16 @@ require('lazy').setup({
 
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
+    lazy = true,
     dependencies = {
       -- Sources
-      'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      -- Awesome Snippets
+      'rafamadriz/friendly-snippets',
       -- VScode-like pictograms
-      'onsails/lspkind.nvim'
+      'onsails/lspkind.nvim',
     },
   },
 
@@ -73,15 +81,17 @@ require('lazy').setup({
   { -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
+    lazy = true,
   },
 
   { -- Tab
     'akinsho/bufferline.nvim',
+    lazy = true,
     dependencies = {'nvim-tree/nvim-web-devicons'}
   },
 
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' } },
+  { 'nvim-telescope/telescope.nvim', lazy = true, version = '*', dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' } },
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built.
   -- Only load if `make` is available. Make sure you have the system
@@ -90,6 +100,7 @@ require('lazy').setup({
     'nvim-telescope/telescope-fzf-native.nvim',
     -- NOTE: If you are having trouble with this installation,
     --       refer to the README for telescope-fzf-native for more instructions.
+    lazy = true,
     build = 'make',
     cond = function()
       return vim.fn.executable 'make' == 1
@@ -97,7 +108,9 @@ require('lazy').setup({
   },
 
   { -- Highlight, edit, and navigate code
+    -- When you see some erros, try `:cheackhelath nvim-treesitter`, and `:TSInstall! <lang-name>`
     'nvim-treesitter/nvim-treesitter',
+    lazy = true,
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
@@ -106,28 +119,29 @@ require('lazy').setup({
     end,
   },
   -- adds indentation guides to all lines (including empty lines)
-  { "lukas-reineke/indent-blankline.nvim" },
+  { "lukas-reineke/indent-blankline.nvim", lazy = true },
 
   { -- EasyMotion
     'phaazon/hop.nvim',
+    lazy = true,
     branch = 'v2', -- optional but strongly recommended
   },
 
   -- GitHub Copilot
   -- It needs tab key is not mapped by other plugins
-  { 'github/copilot.vim' },
+  { 'github/copilot.vim', lazy = true },
 
   -- Display a number of serach results
-  { 'kevinhwang91/nvim-hlslens' },
+  { 'kevinhwang91/nvim-hlslens', lazy = true },
 
   -- Awesome autopair plugin
-  { 'windwp/nvim-autopairs' },
+  { 'windwp/nvim-autopairs', lazy = true },
   -- Also autopair for html tags
-  { 'windwp/nvim-ts-autotag' },
+  { 'windwp/nvim-ts-autotag', lazy = true },
 
   -- Adds git releated signs to the gutter, as well as utilities for managing changes
-  { 'lewis6991/gitsigns.nvim' },
+  { 'lewis6991/gitsigns.nvim', lazy = true },
 
   -- Discord Rich Presence 
-  { 'andweeb/presence.nvim' },
+  { 'andweeb/presence.nvim', lazy = true },
 }, {})
