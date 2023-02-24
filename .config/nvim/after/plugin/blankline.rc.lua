1b994152df12a046824c96bd67223895840ae51d
see2et <seezet1024@gmail.com>
2023-02-25 07:10:21 +0900

    add: eslint code_actions to sources of null-ls

--- .config/nvim/after/plugin/blankline.rc.lua
+++ .config/nvim/after/plugin/blankline.rc.lua
@@ -1,7 +1,7 @@
-local status, indent_blanline = pcall(require, 'indent_blankline')
+local status, indent_blankline = pcall(require, 'indent_blankline')
 if not status then print('indent_blanline is not installed') return end
 
-indent_blanline.setup {
+indent_blankline.setup {
   show_current_context = true,
   show_current_context_start = true,
 }
