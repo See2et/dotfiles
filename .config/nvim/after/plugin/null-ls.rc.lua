0d2a0b44c8657aa4d1c70465aa587076eef8ff78
see2et <seezet1024@gmail.com>
2023-02-25 07:24:48 +0900

    hmm: eslint on null-ls does'nt work well

--- .config/nvim/after/plugin/null-ls.rc.lua
+++ .config/nvim/after/plugin/null-ls.rc.lua
@@ -3,9 +3,11 @@ if not status then print('null-ls is not installed') return end
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
-    }),
-    null_ls.builtins.diagnostics.fish
+    -- null_ls.builtins.diagnostics.eslint.with({
+    --   diagnostics_format = '[eslint] #{m}\n(#{c})'
+    -- }),
+    -- null_ls.builtins.code_actions.eslint,
+    -- null_ls.builtins.formatting.prettier,
+    null_ls.builtins.diagnostics.fish,
   }
 })
