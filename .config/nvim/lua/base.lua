a64ccdf180eb47c2c237041e7505442dd0ab4800
see2et <seezet1024@gmail.com>
2023-02-25 14:23:33 +0900

    apply: cursorline

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -7,6 +7,7 @@ vim.opt.fileencoding = 'utf-8'
 vim.wo.number = false
 vim.wo.relativenumber = false
 
+vim.opt.cursorline = true
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.hlsearch = true
