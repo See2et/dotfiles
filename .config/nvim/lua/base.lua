949271e382774d7105f0bac66576d452b18f803d
see2et <seezet1024@gmail.com>
2023-02-25 13:47:18 +0900

    try: no-number, no-relativenumber

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -4,8 +4,8 @@ vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.wo.number = false
+vim.wo.relativenumber = false
 
 vim.opt.title = true
 vim.opt.autoindent = true
