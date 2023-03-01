d5c72d97db6f040b8b6eba10144805169e42eea6
see2et <seezet1024@gmail.com>
2023-03-01 19:34:25 +0900

    change: map for telekasten panel

--- .config/nvim/after/plugin/which-key.rc.lua
+++ .config/nvim/after/plugin/which-key.rc.lua
@@ -47,7 +47,7 @@ wk.register({
     -- t = {'<cmd>ToggleTerm<cr>', { silent = true}},
     w = { '<cmd>w<cr>', ':[w]' },
     z = {
-      z = { '<cmd>Telekasten panel<cr>', '[Z]ettelkasten' },
+      [''] = { '<cmd>Telekasten panel<cr>', '[Z]ettelkasten' },
       f = { '<cmd>Telekasten find_notes<cr>', '[Z]ettelkasten [F]ind' },
       g = { '<cmd>Telekasten search_notes<cr>', '[Z]ettelkasten [G]rep' },
       d = { '<cmd>Telekasten goto_today<cr>', '[Z]ettelkasten [D]ate' },
