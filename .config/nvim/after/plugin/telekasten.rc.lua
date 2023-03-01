4583924fe9677113297bb8aa672de5f8c9ae4c87
see2et <seezet1024@gmail.com>
2023-03-01 19:40:54 +0900

    telekasten relative path doesn't work

--- .config/nvim/after/plugin/telekasten.rc.lua
+++ .config/nvim/after/plugin/telekasten.rc.lua
@@ -2,5 +2,6 @@ local status, telekasten = pcall(require, 'telekasten')
 if not status then print('telekasten is not installed') return end
 
 telekasten.setup {
-  home = '~/zettelkasten'
+  -- idk reason, but telekasten doesn't read a relative path correctly
+  home =  '/Users/see2et/Documents/telekasten',
 }
