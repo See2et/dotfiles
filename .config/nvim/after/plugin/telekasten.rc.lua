58ae9d0bf1bfa530d5dabe10d7f2c98fc08d718f
see2et <seezet1024@gmail.com>
2023-03-01 19:32:42 +0900

    update: telekasten.rc.lua

--- .config/nvim/after/plugin/telekasten.rc.lua
+++ .config/nvim/after/plugin/telekasten.rc.lua
@@ -2,5 +2,5 @@ local status, telekasten = pcall(require, 'telekasten')
 if not status then print('telekasten is not installed') return end
 
 telekasten.setup {
-  home = '~/Documents/telekasten'
+  home = '~/zettelkasten'
 }
