04f8c270c89321bab45be3b5760d99651db191a7
see2et <seezet1024@gmail.com>
2023-03-01 19:49:14 +0900

    apply: telekasten find theme as dropdown

--- .config/nvim/after/plugin/telekasten.rc.lua
+++ .config/nvim/after/plugin/telekasten.rc.lua
@@ -4,4 +4,5 @@ if not status then print('telekasten is not installed') return end
 telekasten.setup {
   -- idk reason, but telekasten doesn't read a relative path correctly
   home =  '/Users/see2et/Documents/telekasten',
+  command_palette_theme = 'dropdown',
 }
