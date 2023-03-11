676403e63b668edb1244b86f7343b657a448c4c2
see2et <seezet1024@gmail.com>
2023-03-11 17:01:51 +0900

    fix: home of telekasten

--- .config/nvim/lua/config/telekasten.lua
+++ .config/nvim/lua/config/telekasten.lua
@@ -1,8 +1,16 @@
 local status, telekasten = pcall(require, 'telekasten')
-if not status then print('telekasten is not installed') return end
+if not status then
+  print('telekasten is not installed')
+  return
+end
+
+local home = vim.fn.expand("~/Documents/40_2nd-brain")
 
 telekasten.setup {
-  -- idk reason, but telekasten doesn't read a relative path correctly
-  home =  '/Users/see2et/Documents/telekasten',
-  command_palette_theme = 'dropdown',
+    -- idk reason, but telekasten doesn't read a relative path correctly
+    home                  = home,
+    dailies               = home .. '/' .. 'Calendar',
+    weeklies              = home .. '/' .. 'Calendar',
+    templates             = home .. '/' .. 'Extras' .. '/' .. 'Templates',
+    command_palette_theme = 'dropdown',
 }
