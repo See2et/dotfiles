a035fcbb54586c2249ff108d3a961693fe8394fb
see2et <seezet1024@gmail.com>
2023-08-30 02:08:14 +0900

    add hotkeys for obsidian and arc browser

--- .hammerspoon/hotkeys.lua
+++ .hammerspoon/hotkeys.lua
@@ -12,7 +12,35 @@
     win:focus()
   end
 end)]]
-       --
+--
+
+hs.hotkey.bind({ "ctrl" }, "s", function()
+  local app = hs.application.get("Obsidian")
+  if app == nil then
+    hs.application.launchOrFocus("/Applications/Obsidian.app")
+  elseif app:isFrontmost() then
+    app:hide()
+  else
+    local space = hs.spaces.focusedSpace()
+    local win = app:focusedWindow()
+    hs.spaces.moveWindowToSpace(win, space)
+    win:focus()
+  end
+end)
+
+hs.hotkey.bind({ "ctrl" }, "d", function()
+  local app = hs.application.get("Arc")
+  if app == nil then
+    hs.application.launchOrFocus("/Applications/Arc.app")
+  elseif app:isFrontmost() then
+    app:hide()
+  else
+    local space = hs.spaces.focusedSpace()
+    local win = app:focusedWindow()
+    hs.spaces.moveWindowToSpace(win, space)
+    win:focus()
+  end
+end)
 
 -- To get a keyboard id, use hs.keycodes.currentSourceID() on hammerspoon terminal
 hs.hotkey.bind({ "ctrl", "alt" }, "j", function()
