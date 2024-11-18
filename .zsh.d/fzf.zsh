234ff0ad31b55f1be59529a2a2c37a625c278dda
see2et <seezet1024@gmail.com>
2024-11-18 13:41:42 +0900

    fzf and so on

--- .zsh.d/fzf.zsh
+++ .zsh.d/fzf.zsh
@@ -1,7 +1,7 @@
 # fzf
 
 cd-fzf-find() {
-    local DIR=$(find ./ -path '*/\.*' -name .git -prune -o -type d -print 2> /dev/null | fzf +m)
+    local DIR=$(find ./ -path '*/\.*' -name .git -prune -o -type d -print 2> /dev/null | fzf-tmux -p 80% +m)
     if [ -n "$DIR" ]; then
         cd $DIR
     fi
@@ -9,7 +9,7 @@ cd-fzf-find() {
 alias fd=cd-fzf-find
 
 vim-fzf-find() {
-    local FILE=$(find ./ -path '*/\.*' -name .git -prune -o -type f -print 2> /dev/null | fzf +m)
+    local FILE=$(find ./ -path '*/\.*' -name .git -prune -o -type f -print 2> /dev/null | fzf-tmux -p 80% +m)
     if [ -n "$FILE" ]; then
         ${EDITOR:-nvim} $FILE
     fi
@@ -18,7 +18,7 @@ alias fv=vim-fzf-find
 
 function cd-fzf-ghqlist() {
     local GHQ_ROOT=`ghq root`
-    local REPO=`ghq list -p | sed -e 's;'${GHQ_ROOT}/';;g' |fzf +m`
+    local REPO=`ghq list -p | sed -e 's;'${GHQ_ROOT}/';;g' |fzf-tmux -p 80% +m`
     if [ -n "${REPO}" ]; then
         BUFFER="cd ${GHQ_ROOT}/${REPO}"
     fi
@@ -28,7 +28,7 @@ zle -N cd-fzf-ghqlist
 bindkey '^G' cd-fzf-ghqlist
 
 function checkout-fzf-gitbranch() {
-    local GIT_BRANCH=$(git branch --all | grep -v HEAD | fzf +m)
+    local GIT_BRANCH=$(git branch --all | grep -v HEAD | fzf-tmux -p 80% +m)
     if [ -n "$GIT_BRANCH" ]; then
         git checkout $(echo "$GIT_BRANCH" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
     fi
@@ -38,7 +38,7 @@ zle -N checkout-fzf-gitbranch
 bindkey '^O' checkout-fzf-gitbranch
 
 function buffer-fzf-history() {
-    local HISTORY=$(history -n -r 1 | fzf +m)
+    local HISTORY=$(history -n -r 1 | fzf-tmux -p 80% +m)
     BUFFER=$HISTORY
     if [ -n "$HISTORY" ]; then
         CURSOR=$#BUFFER
--- .zshrc
+++ .zshrc
@@ -1,7 +1,5 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-typeset -g POWERLEVEL9K_INSTANT_PROMPT=off # see https://github.com/romkatv/powerlevel10k#instant-prompt
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc. Initialization code that may require console input (password prompts, [y/n] confirmations, etc.) must go above this block; everything else may go below. 
+typeset -g POWERLEVEL9K_INSTANT_PROMPT=off # see https://github.com/romkatv/powerlevel10k#instant-prompt 
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
@@ -32,6 +30,7 @@ if [ -d $ZSH_DIR ] && [ -r $ZSH_DIR ] && [ -x $ZSH_DIR ]; then
         [ -r $file ] && source $file
     done
 fi
+
 
 # PATH
 export PATH="$HOME/.local/bin:$PATH"
@@ -87,7 +86,8 @@ antigen apply
 # zabrze
 eval "$(zabrze init --bind-keys)"
 
-
+# fzf-tmux
+export FZF_TMUX_OPTS="-p 80%"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
