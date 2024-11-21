83a48a979b04289b9266537ef349802af5b99da6
See2et / しーぜっと <me.see2et@gmail.com>
2024-11-22 03:21:11 +0900

    add lsp of typst

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -63,7 +63,8 @@ return {
             },
             omnisharp = {},
             rust_analyzer = {},
-            taplo = {}
+            taplo = {},
+            tinymist = {}
         }
 
         mason.setup()
